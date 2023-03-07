d6d675bcdc2758e0a4d711e356613edbf71d9a12
asif0927 <6zd6jws@code.edu.az>
2023-03-07 11:15:15 +0400

    c# power2 alghoritm created

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int num = int.Parse(Console.ReadLine());
+int powerful = 1;
+int result = 2;
+while (powerful <= 100 && result != num)
+{
+    result *= 2;
+    powerful++;
+}
+if (result == num)
+{
+    Console.WriteLine("2-nin quvvetidir");
+}
+else
+{
+    Console.WriteLine("2-nin quvveti deyil");
+}
